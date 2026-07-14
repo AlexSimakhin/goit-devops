860eaebfa542598e307450dbd3775a09584f8c83
AlexSimakhin <oleksandrsimakhin@gmail.com>
2026-07-14 11:46:14 +0300

    fix: Update Django settings and deployment configurations for PostgreSQL integration

--- app/core/settings.py
+++ app/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$hvl1p6a_1q$cs5o1tx29a3i@0k-dqam#hyowhx950mtr25#xc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
